55cab6f7a620fce513d95f0ef92daa9704ce2ffb
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-08-03 13:37:34 -0700

    feat: terraSysdig
    The silver sand box alerts are stored as code

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,8 @@ module "eb75ad-team" {
   source                   = "./eb75ad-team"
   sysdig_monitor_api_token = var.eb75ad_team_sysdig_monitor_api_token
 }
+
+module "sand3d5c3f-team" {
+  source                   = "./sand3d5c3f-team"
+  sysdig_monitor_api_token = var.sand3d5c3f_team_sysdig_monitor_api_token
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,7 @@ variable "c6af30_team_sysdig_monitor_api_token" {
 variable "eb75ad_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in eb75ad-team workspace"
 }
+
+variable "sand3d5c3f_team_sysdig_monitor_api_token" {
+  description = "The Sysdig Monitor API token in 3d5c3f-team workspace"
+}
