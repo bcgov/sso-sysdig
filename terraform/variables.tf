ebd7bb2c94c56d391d2675fb7b00a9c72ff179e2
thegentlemanphysicist <jon.w.sharman@gmail.com>
2023-05-25 16:17:19 -0700

    Revert "chore: create rocketchat webhook in sandbox (#35)"
    This reverts commit dc370b93e568246d5b64fc100a9c488c04dceaad.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "gold_c6af30_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in c6af30-team workspace"
 }
+
 variable "gold_eb75ad_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in eb75ad-team workspace"
 }
@@ -11,8 +12,4 @@ variable "silver_6d70e7_team_sysdig_monitor_api_token" {
 
 variable "silver_3d5c3f_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in 3d5c3f-team workspace"
-}
-
-variable "rocket_chat_webhook" {
-  description = "The webhook used to connect to sso-alerts channel."
 }
