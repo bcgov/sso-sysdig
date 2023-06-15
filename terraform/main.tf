1248f7587460d90e53616a84d19694dd63a72054
thegentlemanphysicist <jon.w.sharman@gmail.com>
2023-06-15 14:08:24 -0700

    chore: delete silver integrations part 2 (#41)
    * chore: remove silver alerts and dashboards from terraform
    
    * chore: delete silver sandbox terraform config
    
    * chore: delete silver sandbox terraform config
    
    * chore: remove silver prod terraform integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,3 @@ module "eb75ad-team" {
   source                   = "./gold-eb75ad-team"
   sysdig_monitor_api_token = var.gold_eb75ad_team_sysdig_monitor_api_token
 }
-
-module "silver-3d5c3f-team" {
-  source                   = "./silver-3d5c3f-team"
-  sysdig_monitor_api_token = var.silver_3d5c3f_team_sysdig_monitor_api_token
-}
-
-module "silver-6d70e7-team" {
-  source                   = "./silver-6d70e7-team"
-  sysdig_monitor_api_token = var.silver_6d70e7_team_sysdig_monitor_api_token
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,11 +5,3 @@ variable "gold_c6af30_team_sysdig_monitor_api_token" {
 variable "gold_eb75ad_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in eb75ad-team workspace"
 }
-
-variable "silver_6d70e7_team_sysdig_monitor_api_token" {
-  description = "The Sysdig Monitor API token in 6d70e7-team workspace"
-}
-
-variable "silver_3d5c3f_team_sysdig_monitor_api_token" {
-  description = "The Sysdig Monitor API token in 3d5c3f-team workspace"
-}
