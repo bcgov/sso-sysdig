359663adfa132fb78d71dd4dfc940bd70af4a0ff
Jonathan Sharman <jon.w.sharman@gmail.com>
2023-06-15 12:55:49 -0700

    chore: remove silver prod terraform integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,3 @@ module "eb75ad-team" {
   source                   = "./gold-eb75ad-team"
   sysdig_monitor_api_token = var.gold_eb75ad_team_sysdig_monitor_api_token
 }
-
-module "silver-6d70e7-team" {
-  source                   = "./silver-6d70e7-team"
-  sysdig_monitor_api_token = var.silver_6d70e7_team_sysdig_monitor_api_token
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,3 @@ variable "gold_c6af30_team_sysdig_monitor_api_token" {
 variable "gold_eb75ad_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in eb75ad-team workspace"
 }
-
-variable "silver_6d70e7_team_sysdig_monitor_api_token" {
-  description = "The Sysdig Monitor API token in 6d70e7-team workspace"
-}
