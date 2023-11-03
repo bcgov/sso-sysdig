de5dd012136c57829dabbf7bb52f44b8fd9f7524
Jonathan Sharman <jon.w.sharman@gmail.com>
2023-11-03 11:04:28 -0700

    chore: create the terraform integration with new sandbox

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,7 @@ variable "gold_c6af30_team_sysdig_monitor_api_token" {
 variable "gold_eb75ad_team_sysdig_monitor_api_token" {
   description = "The Sysdig Monitor API token in eb75ad-team workspace"
 }
+
+variable "gold_e4ca1d_team_sysdig_monitor_api_token" {
+  description = "The Sysdig Monitor API token in e4ca1d-team workspace"
+}
