cd0c05c42919899fb53d8ca17a79fc103989e6fc
thegentlemanphysicist <jon.w.sharman@gmail.com>
2024-04-30 10:55:05 -0700

    chore: change name and description of filesystem tests (#59)
    * chore: change name and description of filesystem tests
    
    * chore: fix container pvc alerts to directly check the pvc

--- terraform/gold-eb75ad-team/alerts.tf
+++ terraform/gold-eb75ad-team/alerts.tf
@@ -127,8 +127,8 @@ resource "sysdig_monitor_alert_metric" "prod_keycloak_pods_low" {
 }
 
 resource "sysdig_monitor_alert_metric" "prod_keycloak_log_pv_high" {
-  name        = "[GOLD CUST PROD] SSO - Log PV Usage over 90%"
-  description = ""
+  name        = "[GOLD CUST PROD] SSO - Keycloak Filesystem usage over 90%"
+  description = "This container filesystem is filling up."
   severity    = 0
   enabled     = true
 
@@ -145,8 +145,8 @@ resource "sysdig_monitor_alert_metric" "prod_keycloak_log_pv_high" {
 }
 
 resource "sysdig_monitor_alert_metric" "prod_keycloak_log_pv_med" {
-  name        = "[GOLD CUST PROD] SSO - Log PV Usage over 70%"
-  description = ""
+  name        = "[GOLD CUST PROD] SSO - Keycloak Filesystem Usage over 70%"
+  description = "This container filesystem is filling up."
   severity    = 4
   enabled     = true
 
@@ -216,17 +216,14 @@ resource "sysdig_monitor_alert_metric" "prod_db_pods_low" {
   }
 }
 
-resource "sysdig_monitor_alert_metric" "prod_backup_storage_pv_usage_gt_med" {
+resource "sysdig_monitor_alert_promql" "prod_backup_storage_pv_usage_gt_med" {
   name        = "[GOLD CUST PROD] DB Backup - storage 80%"
-  description = ""
+  description = "This uses the filesystem as a proxy for PVC space."
   severity    = 4
   enabled     = true
+  promql      = "avg(kubelet_volume_stats_used_bytes{namespace=\"eb75ad-prod\", persistentvolumeclaim=~\"sso-backup-storage-backup-pvc\"}*100 / kubelet_volume_stats_capacity_bytes{namespace=\"eb75ad-prod\", persistentvolumeclaim=~\"sso-backup-storage-backup-pvc\"}) by (persistentvolumeclaim) > 80"
 
-  metric                = "max(avg(sysdig_container_fs_used_percent)) > 80"
   trigger_after_minutes = 2
-
-  scope              = "kubernetes.cluster.name in (\"gold\") and kubernetes.namespace.name in (\"eb75ad-prod\") and kubernetes.deployment.name in (\"sso-backup-storage\")"
-  multiple_alerts_by = []
 
   notification_channels = [132277, 57336, 57341]
   custom_notification {
@@ -234,17 +231,14 @@ resource "sysdig_monitor_alert_metric" "prod_backup_storage_pv_usage_gt_med" {
   }
 }
 
-resource "sysdig_monitor_alert_metric" "dev_backup_storage_pv_usage_gt_med" {
+resource "sysdig_monitor_alert_promql" "dev_backup_storage_pv_usage_gt_med" {
   name        = "[GOLD CUST DEV] DB Backup - storage 85%"
-  description = ""
+  description = "This uses the filesystem as a proxy for PVC space."
   severity    = 4
   enabled     = true
 
-  metric                = "max(avg(sysdig_container_fs_used_percent)) > 85"
+  promql                = "avg(kubelet_volume_stats_used_bytes{namespace=\"eb75ad-dev\", persistentvolumeclaim=~\"sso-backup-storage-backup-pvc\"}*100 / kubelet_volume_stats_capacity_bytes{namespace=\"eb75ad-dev\", persistentvolumeclaim=~\"sso-backup-storage-backup-pvc\"}) by (persistentvolumeclaim) > 85"
   trigger_after_minutes = 2
-
-  scope              = "kubernetes.cluster.name in (\"gold\") and kubernetes.namespace.name in (\"eb75ad-dev\") and kubernetes.deployment.name in (\"sso-backup-storage\")"
-  multiple_alerts_by = []
 
   notification_channels = [132277, 57336, 57341]
   custom_notification {
@@ -252,17 +246,14 @@ resource "sysdig_monitor_alert_metric" "dev_backup_storage_pv_usage_gt_med" {
   }
 }
 
-resource "sysdig_monitor_alert_metric" "test_backup_storage_pv_usage_gt_med" {
+resource "sysdig_monitor_alert_promql" "test_backup_storage_pv_usage_gt_med" {
   name        = "[GOLD CUST TEST] DB Backup - storage 85%"
-  description = ""
+  description = "This uses the filesystem as a proxy for PVC space."
   severity    = 4
   enabled     = true
 
-  metric                = "max(avg(sysdig_container_fs_used_percent)) > 85"
+  promql                = "avg(kubelet_volume_stats_used_bytes{namespace=\"eb75ad-test\", persistentvolumeclaim=~\"sso-backup-storage-backup-pvc\"}*100 / kubelet_volume_stats_capacity_bytes{namespace=\"eb75ad-test\", persistentvolumeclaim=~\"sso-backup-storage-backup-pvc\"}) by (persistentvolumeclaim) > 85"
   trigger_after_minutes = 2
-
-  scope              = "kubernetes.cluster.name in (\"gold\") and kubernetes.namespace.name in (\"eb75ad-test\") and kubernetes.deployment.name in (\"sso-backup-storage\")"
-  multiple_alerts_by = []
 
   notification_channels = [132277, 57336, 57341]
   custom_notification {
