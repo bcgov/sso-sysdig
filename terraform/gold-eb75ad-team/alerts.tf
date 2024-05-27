6649719bc3d056702886c3482332c6eb0a94a0d0
Jonathan Sharman <jon.w.sharman@gmail.com>
2024-05-27 12:22:23 -0700

    chore: move change alert to alert file

--- terraform/gold-eb75ad-team/alerts.tf
+++ terraform/gold-eb75ad-team/alerts.tf
@@ -623,3 +623,42 @@ resource "sysdig_monitor_alert_promql" "test_gold_memory_limit" {
     title = "{{__alert_name__}} is {{__alert_status__}}"
   }
 }
+
+resource "sysdig_monitor_alert_v2_change" "dev_pvcuse_spike" {
+
+  name              = "[DEV] the dev PVC usage surged(EXPERIMENT)"
+  severity          = "low"
+  metric            = "kubelet_volume_stats_used_bytes"
+  group_aggregation = "avg"
+  time_aggregation  = "avg"
+  operator          = ">"
+  threshold         = 3
+  group_by          = [""]
+
+  scope {
+    label    = "kube_cluster_name"
+    operator = "equals"
+    values   = ["gold"]
+  }
+
+  scope {
+    label    = "kube_namespace_name"
+    operator = "equals"
+    values   = ["eb75ad-dev"]
+  }
+
+  scope {
+    label    = "persistentvolumeclaim"
+    operator = "in"
+    values   = ["storage-volume-sso-patroni-0", "storage-volume-sso-patroni-1", "storage-volume-sso-patroni-2"]
+  }
+
+  notification_channels {
+    id                     = 132277
+    renotify_every_minutes = 60
+  }
+
+  shorter_time_range_seconds = 600
+  longer_time_range_seconds  = 28800
+
+}
