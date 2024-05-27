08fde502a58574e8e0537c6c69536ccf3457addb
Jonathan Sharman <jon.w.sharman@gmail.com>
2024-05-27 10:56:01 -0700

    chore: experiment sysdig surge limit

--- terraform/gold-eb75ad-team/changealerts.tf
+++ terraform/gold-eb75ad-team/changealerts.tf
@@ -1,32 +1,38 @@
-resource "sysdig_monitor_alert_v2_change" "sample" {
+resource "sysdig_monitor_alert_v2_change" "dev_pvcuse_spike" {
 
-  name              = "high cpu used compared to previous periods"
-  severity          = "high"
-  metric            = "sysdig_container_cpu_used_percent"
+  name              = "[DEV] the dev PVC usage surged(EXPERIMENT)"
+  severity          = "low"
+  metric            = "kubelet_volume_stats_used_bytes"
   group_aggregation = "avg"
   time_aggregation  = "avg"
   operator          = ">"
-  threshold         = 75
-  group_by          = ["kube_pod_name"]
+  threshold         = 3
+  group_by          = [""]
 
   scope {
     label    = "kube_cluster_name"
-    operator = "in"
-    values   = ["my_cluster_1", "my_cluster_2"]
+    operator = "equals"
+    values   = ["gold"]
   }
 
   scope {
-    label    = "kube_deployment_name"
+    label    = "kube_namespace_name"
     operator = "equals"
-    values   = ["my_deployment"]
+    values   = ["eb75ad-dev"]
+  }
+
+  scope {
+    label    = "persistentvolumeclaim"
+    operator = "in"
+    values   = ["storage-volume-sso-patroni-0", "storage-volume-sso-patroni-1", "storage-volume-sso-patroni-2"]
   }
 
   notification_channels {
-    id                     = 1234
+    id                     = 132277
     renotify_every_minutes = 60
   }
 
-  shorter_time_range_seconds = 300
-  longer_time_range_seconds  = 3600
+  shorter_time_range_seconds = 599
+  longer_time_range_seconds  = 28799
 
 }
